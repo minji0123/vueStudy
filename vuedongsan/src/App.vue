3b9df4ce71b0eb208b858a59de50e35bddd97d1b
minji0123 <sso02027@naver.com>
2023-03-12 22:28:51 +0900

    뷰 수정

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -14,21 +14,30 @@
     <p>{{ price1 }} 만원</p>
   </div> -->
 
-  <div>  
+  <div>
+    <img src="./assets/room0.jpg" >
     <h4>{{ products[0] }}</h4>
     <p>{{ price1 }} 만원</p>
-    <button v-on:click="increase">허위 매몰 신고</button>
-    <button @click="increase">허위 매몰 신고</button>
-    <span>신고수 : {{신고수}} </span>
+    <!-- <button v-on:click="increase">허위 매몰 신고</button> -->
+    <button @click="increase(0)">허위 매몰 신고</button>
+    <span>신고수 : {{신고수[0]}} </span>
   </div>
 
   <div>
+    <img src="./assets/room1.jpg" >
     <h4>{{ products[1] }}</h4>
     <p>{{ price2 }} 만원</p>
+    <button @click="increase(1)">허위 매몰 신고</button>
+    <span>신고수 : {{신고수[1]}} </span>
+
   </div>
   <div>
+    <img src="./assets/room2.jpg" >
     <h4>{{ products[2] }}</h4>
     <p>{{ price2 }} 만원</p>
+    <button @click="increase(2)">허위 매몰 신고</button>
+    <span>신고수 : {{신고수[2]}} </span>
+
   </div>
 
 </template>
@@ -44,15 +53,15 @@ export default {
       price1: 60,
       price2: 70,
       스타일: 'color : #2c3e50',
-      신고수: 0,
+      신고수: [0,0,0],
       메뉴들: ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포동원룸']
     }
 
   },
   methods : {
-    increase(){
-      this.신고수++;
+    increase(i){
+      this.신고수[i]++;
     }
   },
   components: {
