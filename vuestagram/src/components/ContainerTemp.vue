3a7da28203c05be914a9e99045839f4be5be8dbc
minji0123 <sso02027@naver.com>
2023-03-31 18:00:53 +0900

    프로젝트 시작

---
+++ vuestagram/src/components/ContainerTemp.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div>
+
+    <PostTemp :postData="postData[i]"
+              v-for="(a,i) in postData" :key="i"
+              />
+
+  </div>
+
+</template>
+
+<script>
+import PostTemp from './PostTemp.vue'
+
+export default {
+  name: 'ContainerTemp',
+  props:{
+    postData : Array,
+  },
+  components: {
+    PostTemp,
+  }
+}
+</script>
+
+<style>
+</style>
